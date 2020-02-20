22aff90894b231f319752391b220b53bc7528da5
robert <rakont666@gmail.com>
2020-02-20 16:22:29 +0100

    New file health_checks1.py added

---
+++ health_checks1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import sys
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists(("/run/reboot-required"))
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Return True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabyt
+    gigabytes_free = du.free / 2**30
+    if gigabytes_free < min_gb or percent_free < min_percent:
+        return True
+    return False
+
+def check_root_full():
+    """Return True if root partition is full """
+    return check_disk_full(disk="/",min_gb = 2, min_percent = 10)
+
+def main():
+    checks = [(check_reboot, "Pending Reboot."),
+              (check_root_full, "Root partition full")]
+    everything_ok = True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
+            everything_ok = False
+
+    if not everything_ok:
+        sys.exit(1)
+    print("Everything OK !!! MASTER")
+    sys.exit(0)
+
+main()
+
